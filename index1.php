196ddf2e269709e572158dd88ece770494dc2547
GOKUL REDDY <59329253+GOKUL-REDDY@users.noreply.github.com>
2020-10-15 12:39:05 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])){
     $contact = $_POST['number'];
     $message = $_POST['message'];
 
-    $mailTo = "ceo@sirivanaminfra.com";
+    $mailTo = "gokulkrishna.talla@gmail.com";
     $headers = "From: ".$mailFrom."\n\nName: ".$name."\n\n Message: ".$message."\n\n Contact: ".$contact;
     $txt = "Enquiry e-Mail";
 
